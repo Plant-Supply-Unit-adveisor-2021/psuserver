0e26f9949e9831793e46f83909c8042cb5aaafaa
MaxTech <maxtech@t-online.de>
2021-02-22 21:46:03 +0100

    added login form

---
+++ authentification/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.password_validation import validate_password, password_validators_help_text_html
+from django.contrib.auth import authenticate
+
+from django.utils.translation import ugettext as _
+
+
+class LoginForm(forms.Form):
+    """
+    Form to login
+    """
+
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise forms.ValidationError(_('Mail and password do not match or your account is not activate yet.'))
+        return self.cleaned_data
+
+    def login(self, request):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        return user
--- authentification/views.py
+++ authentification/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 
+from django.contrib.auth import login, logout
+
+from authentification.forms import LoginForm
+
 # Create your views here.
 
 def login_view(request):
-    return render(request, 'authentification/login.html')
+    """
+    View for Login
+    """
+
+    """
+    ToDo after @login_required becomes a topic
+
+    if 'next' in request.GET:
+        next_page = request.GET['next']
+    else:
+        next_page = "/"
+    """
+
+
+    login_form = LoginForm(request.POST or None)
+    if request.POST and login_form.is_valid():
+        user = login_form.login(request)
+        if user:
+            login(request, user)
+    return render(request, 'authentification/login.html', {'login_form': login_form})
