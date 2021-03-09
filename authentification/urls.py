25a08cdea14fa7d69a147d95c642610ec212fcaf
MaxTech <maxtech@t-online.de>
2021-03-09 18:57:05 +0100

    added registration form and view

--- authentification/urls.py
+++ authentification/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path(r'login', v.login_view, name="login"),
     path(r'logout', v.logout_view, name="logout"),
     path(r'edit_profile', v.edit_profile_view, name="edit_profile"),
+    path(r'register', v.register_view, name="register"),
+    path(r'welcome', v.register_after_view, name="register_after"),
 ]
