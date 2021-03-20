365c7f4e01dd523706b32383f7c778696869b28a
MaxTechCode <59182970+MaxTechCode@users.noreply.github.com>
2021-03-20 11:02:41 +0100

    Remove makemessages and makemigrations

--- django_migrate_n_compile.bat
+++ django_migrate_n_compile.bat
@@ -5,14 +5,12 @@ echo    Apply Migrations
 echo  -------------------------------------
 echo.
 call .\.venv\Scripts\activate
-call python manage.py makemigrations
 call python manage.py migrate
 echo.
 echo  -------------------------------------
 echo    Handle Translations
 echo  -------------------------------------
 echo.
-call django-admin makemessages -l de
 call django-admin compilemessages -l de
 echo.
 echo  -------------------------------------
