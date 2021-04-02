5effc9fb7eb83df8bb193fa2da6cfac3e0d660e3
MaxTechCode <maxtech@t-online.de>
2021-04-02 18:41:43 +0200

    replaced ugettext with gettext

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 from authentication.forms import LoginForm, EditProfileForm, ChangePasswordForm, RegisterForm
--- website/settings.py
+++ website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
