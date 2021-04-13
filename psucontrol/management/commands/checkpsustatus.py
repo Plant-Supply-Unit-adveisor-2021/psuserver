460d2c4126e7bf1550d6235fa0517f4bb18d1eea
MaxTechCode <maxtech@t-online.de>
2021-04-13 11:29:42 +0200

    added logic to check status

--- psucontrol/management/commands/checkpsustatus.py
+++ psucontrol/management/commands/checkpsustatus.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils import timezone
 
-from psucontrol.models import DataMeasurement
+from psucontrol.models import PSU, DataMeasurement
 from website.utils import get_timedelta
 
 
@@ -21,10 +21,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # get lease times
-        delta = get_timedelta(options['time'])
+        max_delta = get_timedelta(options['time'])
 
-        # print lease times to screen
-        self.stdout.write('Allowed offline time: {}'.format(str(delta)))
-        self.stdout.write('Mail used to send out mails:: {}'.format(settings.DEFAULT_FROM_EMAIL))
+        # print lease time and mail to screen
+        self.stdout.write('Allowed offline time: {}'.format(str(max_delta)))
+        self.stdout.write('Mail used to send out mails: {}'.format(settings.DEFAULT_FROM_EMAIL))
 
-        send_mail('TEST SUBJECT', 'HELLO WORLD', from_email=None, recipient_list=['psu@trash-mail.com'])
+        # check psus for last entry
+        for psu in PSU.objects.all():
+            last_entry = DataMeasurement.objects.filter(psu=psu).order_by('-timestamp').first()
+            if last_entry is None:
+                # message for no entry
+                self.stdout.write('Last entry of {}: NONE'.format(str(psu)))
+            else:
+                delta = timezone.now() - last_entry.timestamp
+                if delta > max_delta:
+                    # message for last entry too long ago
+                    self.stdout.write('Last entry of {}: {}, thats {} and too long ago'.format(str(psu), last_entry.timestamp.strftime('%d.%m.%Y %H:%M:%S'), str(delta)))
+                else:
+                    # message for everything is fine
+                    self.stdout.write('Last entry of {}: {}, thats {} ago'.format(str(psu), last_entry.timestamp.strftime('%d.%m.%Y %H:%M:%S'), str(delta)))
