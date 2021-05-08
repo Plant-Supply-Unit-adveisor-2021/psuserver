053d02b2c48c93ac5b78aaf33039e0a77c48d4e2
MaxTechCode <maxtech@t-online.de>
2021-05-08 21:39:53 +0200

    Fixes create view to manually water the plant #49

--- psufrontend/urls.py
+++ psufrontend/urls.py
@@ -6,4 +6,5 @@ app_name = 'psufrontend'
 
 urlpatterns = [
     path(r'register_psu', v.register_psu_view, name='register_psu'),
+    path(r'add_watering_task', v.add_watering_task_view, name='add_watering_task'),
 ]
