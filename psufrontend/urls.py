7d08a4b095077b38e7ecac3ab4c47b0d24639845
MaxTechCode <maxtech@t-online.de>
2021-05-15 13:19:40 +0200

    minor fixes concering code style

--- psufrontend/urls.py
+++ psufrontend/urls.py
@@ -6,8 +6,8 @@ app_name = 'psufrontend'
 
 urlpatterns = [
     path(r'register_psu', v.register_psu_view, name='register_psu'),
-    path(r'table/psu/<int:psu>/', v.table_view, name='table'),
-    path(r'table/', v.table_view, name='table'),
+    path(r'table/psu/<int:psu>', v.table_view, name='table'),
+    path(r'table', v.table_view, name='table'),
     path(r'no_psu', v.no_psu_view, name='no_psu'),
     path(r'add_watering_task', v.add_watering_task_view, name='add_watering_task'),
 ]
