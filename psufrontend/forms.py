c3c9698c9173e50ca9a4fbb026771aad82917ce2
daniel <daniel.vilsthaler@gmail.com>
2021-05-18 09:32:55 +0200

    was entfernt

--- psufrontend/forms.py
+++ psufrontend/forms.py
@@ -27,7 +27,6 @@ class ChangeUserPermissionsForm(forms.Form):
     """
     form for selecting a PSU and changing permissions for active users or add new users with permissions
     """
-    #select_psu = forms.ChoiceField(label=_('Select psu: '), label_suffix=_(''), help_text=_('For which PSU do you want to change the user permissions?'))
 
     active_users = forms.ChoiceField(label=_('Active users: '), label_suffix=_(''),
                                      help_text=_('select permitted user to remove permissions'))
@@ -35,21 +34,12 @@ class ChangeUserPermissionsForm(forms.Form):
     select_user = forms.CharField(label=_('Select user'), max_length=100, min_length=1,
                                   help_text=_('Add a user that you want to give permissions to'))
 
-    # stellt die PSUs, auf die der user Zugriff hat, in einem ChoiceField zur Auswahl
+    # stellt die user, die auf die ausgewählte PSU Zugriff haben, in einem MultipleChoiceField zur Auswahl
     def __init__(self, psus, users, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.psus = psus
-        #self.fields['select_psu'].choices = self.psus_choices()
         self.users = users
         self.fields['active_users'].choices = self.users_choices()
 
-    """def psus_choices(self):
-        strings = []
-        for p in self.psus:
-            strings.append((str(p), p))
-        return strings"""
-
-    # stellt die user, die auf die ausgewählte PSU Zugriff haben, in einem MultipleChoiceField zur Auswahl
     def users_choices(self):
         strings = []
         for u in self.users:
