187e570ddf41cce2bdd4d262e2add360b8def77a
unknown <zhihao.d@yahoo.com>
2021-06-13 16:40:18 +0800

    tiny changes on helptext

--- psufrontend/forms.py
+++ psufrontend/forms.py
@@ -46,8 +46,8 @@ class WateringControlForm(forms.Form):
     """
     form to change watering parameters and choose if you want to water manually
     """
-    params = forms.TypedChoiceField(label=_('Watering Paramter'), choices=[], help_text=_('Choose your watering parameter.'), coerce=to_psu)
-    unauthorized_watering = forms.BooleanField(label='Water PSU manually', required=False, help_text=_('Select if you want to water you PSU yourself'))
+    params = forms.TypedChoiceField(label=_('Watering Paramter'), choices=[], help_text=_('Please choose the correct watering parameter.'), coerce=to_psu)
+    unauthorized_watering = forms.BooleanField(label='Water PSU manually', required=False, help_text=_('Click on the button if you want to water your PSU yourself.'))
 
     def __init__(self, wateringparameters, *args, **kwargs):
         super().__init__(*args, **kwargs)
